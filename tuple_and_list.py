9c93cc346bc253975cdea538ff344379d11a2e33
Myrza11 <bapaevmyrza038@gmail.com>
2021-05-22 14:50:51 +0600

    first commit

---
+++ tuple_and_list.py
@@ -0,0 +1,107 @@
+# # первое задание
+# a = [1,2,3,5,7]
+# b = (176,357)
+# c = (4,9,55)
+# d = (38,809)
+# s = (873,2863,93)
+# a.append(s)
+# a.append(b)
+# a.append(c)
+# a.append(d)
+# a.append(234)
+# a.append(2456)
+# print(a)
+
+
+#втарое задание
+'''a = (123,234,345)
+print(a.index(234))
+print(a.index(345))
+print(a.index(123))
+
+
+#третье задиние
+s = []
+d = (5,6,10)
+f = ('Rust','myrza')
+k =('Hi',3)
+s.append(d)
+s.append(f)
+s.append(k)
+print(s)
+
+
+#четвёртое задание
+name = ['мырза','gorje','Anjelina']
+a = ()
+print("a".join(name))
+
+#пятое задание
+a = [1,2,3,4]
+b = [10,15,20]
+c = a + b
+print(c)
+
+#шестое задание
+names = ['Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon',  'Jimmy', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon',]
+a = names.count('Jack')
+print(a)
+
+#седьмое задание
+names = ['Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon',  'Jimmy', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack', 'Jackson', 'Jhon',]
+a = names.remove('Jack')
+print(a)
+print(names)
+
+
+#восьмое задание
+a = []
+b = ("myrza")
+c = (2006)
+d = ("наверное питон")
+a.append(b)
+a.append(c)
+a.append(d)
+print(a)
+
+
+#девятое заданиe
+pythonList = ["int", "str", "bool", "if", "else", "elif", "loop", "tuple", "list", "None", True, False]
+a = pythonList.remove('loop')print(a)
+print(pythonList)
+
+
+
+#десятое за
+
+numbers = [123, 321, 2, 543, 64, 463, 234, 867, 6234, 63246, 3, 43]
+c = 1
+a = [6, 6, 2, 12, 10, 13, 9, 21, 15, 21, 3, 7]
+print('Произведение чисел:',end='')
+for i in a:c *= i
+print(i,'',end='')
+print('=',c)
+
+'''
+#одиннадцатое задание
+spisok_1 = ('Lamborgini', 17, '4456', 2020, 'Paris', 'USA', 11, 23)
+letters = ()
+for i in range(len(spisok_1))
+
+
+
+
+
+
+
+'''
+#двеннадцетое задание
+pythonList = ["int", "str", "bool", "if", "else", "elif", "loop", "tuple", "list", "None", True, False]
+i = 1
+while i < 12:
+    print(pythonList[i])
+    i+=2
+'''
+ 
+ 
+'''
